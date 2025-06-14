a3f28a4243c10ae8d5b42c0b73d6200a3aae4299
GBado <gbado@gmail.com>
2025-06-13 21:50:51 -0300

    teste compilador atuializado

--- compilar.ps1
+++ compilar.ps1
@@ -1,11 +1,19 @@
 $PATH_TO_FX = "$env:USERPROFILE\javafx\javafx-sdk-21.0.2\lib"
 $OUT_DIR = "out"
+$SRC_DIR = "src"
 
+# Limpeza completa
 Remove-Item -Path $OUT_DIR -Recurse -Force -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Path $OUT_DIR -Force | Out-Null
+
+# Cria estrutura de diretórios
+New-Item -ItemType Directory -Path "$OUT_DIR\ui\views" -Force | Out-Null
+New-Item -ItemType Directory -Path "$OUT_DIR\ui\controllers" -Force | Out-Null
+
+# Copia arquivos FXML e recursos
+Copy-Item -Path "$SRC_DIR\ui\views\*" -Destination "$OUT_DIR\ui\views\" -Recurse -Force
 
 # Compilação
 javac --module-path $PATH_TO_FX --add-modules javafx.controls,javafx.fxml,javafx.base -d $OUT_DIR (Get-ChildItem -Path src -Recurse -Filter "*.java").FullName
 
 # Execução
-java --module-path $PATH_TO_FX --add-modules javafx.controls,javafx.fxml,javafx.base -cp "$OUT_DIR" app.Main
+java --module-path $PATH_TO_FX --add-modules javafx.controls,javafx.fxml,javafx.base --enable-native-access=javafx.graphics -cp "$OUT_DIR" app.Main
